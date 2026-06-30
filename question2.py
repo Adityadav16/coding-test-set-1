4fff18f68b93408df727c1b7bc324ac72361fa0d
Aditya singh yadav <151734046+Adityadav16@users.noreply.github.com>
2026-06-30 11:48:35 +0530

    Add functions to reverse subarrays and perform operations

---
+++ question2.py
@@ -0,0 +1,31 @@
+def reverse_subarray(arr, left, right):
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+
+
+def perform_operations(arr, operations):
+    for left, right in operations:
+        reverse_subarray(arr, left, right)
+    return arr
+
+
+
+if __name__ == "__main__":
+
+    arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+    operations = [
+        (0, 9),
+        (4, 5),
+        (3, 6),
+        (2, 7),
+        (1, 8),
+        (0, 9)
+    ]
+
+    result = perform_operations(arr, operations)
+
+    print("Final Array:")
+    print(result)
